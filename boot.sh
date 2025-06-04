434256b77b29bf9684a7ed704ebec73f9c433c30
Luan Rohwedder <luan-rohwedder@hotmail.com>
2025-06-04 16:23:07 -0400

    :hammer: Change setup and boot

--- boot.sh
+++ boot.sh
@@ -1,4 +1,27 @@
-#!/bin/bash
+set -e
+
+ascii_art='
+ _                        ___           _     
+| |                      / _ \         | |    
+| |     __ _ _____   _  / /_\ \_ __ ___| |__  
+| |    / _` |_  / | | | |  _  |  __/ __|  _ \ 
+| |___| (_| |/ /| |_| | | | | | | | (__| | | |
+\_____/\__,_/___|\__, | \_| |_/_|  \___|_| |_|
+                  __/ |                       
+                 |___/                        
+
+'
+
+echo -e "$ascii_art"
+echo -e "\nPress enter to continue (or abort with ctrl + c)..."
+read
+
+#Installing git if not installed
+if ! command -v git &>/dev/null; then
+  echo "Installing git"
+  sudo pacman -S --noconfirm git
+  echo "git installation completed!"
+fi
 
 git clone https://github.com/luanrohwedder/LazyArch.git ~/.local/share/lazyarch
 bash ~/.local/share/lazyarch/setup-system.sh
--- setup-system.sh
+++ setup-system.sh
@@ -2,26 +2,20 @@ set -e
 
 echo "Update System..."
 sudo pacman -Syu --noconfirm
-
-#Installing git if not installed
-if ! command -v git &>/dev/null; then
-  echo "Installing git"
-  sudo pacman -S --noconfirm git
-  echo "git installation completed!"
-fi
+sudo pacman -S --needed base-devel
 
 #Installing yay if not installed
 if ! command -v yay &>/dev/null; then
-  echo "Installing yay"
   git clone https://aur.archlinux.org/yay.git ~/yay
   cd ~/yay
   makepkg -si --noconfirm
   cd ~
   rm -rf ~/yay
-  echo "yay installation completed!"
 fi
 
 # Install packages
 source ~/.local/share/lazyarch/install/install.sh
 
-echo "Finish installation"
+clear
+echo "Installation completed!"
+echo "Reboot system to apply changes"
